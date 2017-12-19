dc0c06f0d9dd62ebfd4744590ce13f417afe1428
Sapir Elmakayes <sapirelmakayes@gmail.com>
2017-12-19 22:55:00 +0200

    MLP

--- phishing_classifier.py
+++ phishing_classifier.py
@@ -3,31 +3,36 @@ import pandas as pd
 import numpy as np
 import sys
 
+# Data settings
 PHISHING_DATA = "phishing_sites.csv"
 NORMAL_DATA = "normal_sites.csv"
 
+# CSV data structe
 URL_COLUMN_NAME = "url"
+IS_PHISHING_COLUMN_NAME = "is_phishing"
 
+# Features
 FEATURE_URL_LENGTH = "url_length"
 FEATURE_DIGITS_COUNT = "digit_count"
 FEATURE_DOT_COUNT = "dot_count"
 FEATURES = [FEATURE_URL_LENGTH, FEATURE_DIGITS_COUNT, FEATURE_DOT_COUNT]
 
-IS_PHISHING_COLUMN_NAME = "is_phishing"
 
-TRANING_PERCENTAGES = 80
+# Traninig sizes
+DATA_SIZE_FROM_EACH_SOURCE = 4
 TEST_PERCENTAGES = 20
+TRANING_PERCENTAGES = 100 - TEST_PERCENTAGES
 
-DATA_SIZE_FROM_EACH_SOURCE = 10
-
+# Traning settings
 EPSILON = 1e-12
 MAX_STEPS = 10000
-ALPHA = 0.00001
-HIDDEN_LAYERS_NUM = len(FEATURES)
+ALPHA = 0.001
 DEVIATION = 0.1
 BIAS_INIT_NUM = 0.1
 
-THRESHOLD = 0.5
+# Classifier settings
+HIDDEN_LAYERS_COUNT = 3
+THRESHOLD = 0.4
 
 def shuffle(dataFrame):
 	return dataFrame.sample(frac = 1)
@@ -83,7 +88,7 @@ def splitData(data):
 
 	return (training_data, test_data)
 
-def generateClassifier(training_data):
+def generateNeuralNetworkClassifier(training_data):
 	print "Start training setup"
 
 	(training_rows, training_cols) = training_data.shape
@@ -92,12 +97,12 @@ def generateClassifier(training_data):
 	x = tf.placeholder(tf.float32, shape = (training_rows, num_of_features))
 	y_ = tf.placeholder(tf.float32, shape = (training_rows,))
 
-	W1 = tf.Variable(tf.truncated_normal([num_of_features, HIDDEN_LAYERS_NUM], stddev = DEVIATION), name = "W_matrix")
-	b1 = tf.Variable(tf.constant(BIAS_INIT_NUM, shape = [HIDDEN_LAYERS_NUM]), name = "b_vector")
+	W1 = tf.Variable(tf.truncated_normal([num_of_features, HIDDEN_LAYERS_COUNT], stddev = DEVIATION), name = "W_matrix")
+	b1 = tf.Variable(tf.constant(BIAS_INIT_NUM, shape = [HIDDEN_LAYERS_COUNT]), name = "b_vector")
 	z1 = tf.add(tf.matmul(x, W1), b1)
 
 	a1 = tf.nn.relu(z1)
-	W2 = tf.Variable(tf.truncated_normal([HIDDEN_LAYERS_NUM, 1], stddev = DEVIATION), name = "W")
+	W2 = tf.Variable(tf.truncated_normal([HIDDEN_LAYERS_COUNT, 1], stddev = DEVIATION), name = "W")
 	b2 = tf.Variable(0., name = "b")
 	z2 = tf.matmul(a1, W2) + b2
 
@@ -142,24 +147,49 @@ def generateClassifier(training_data):
 
 	print "Training on the data completed successfully"
 
-	return (session, W2, b2)
+	return (session, W1, b1, W2, b2)
 
 def sigmoid(t):
 	return 1 / (1.0 + np.exp(-t))
 
 def predict(classifier, test):
-	(session, W, b) = classifier
-	return sigmoid(np.matmul([test], session.run(W)) + session.run(b))[0][0]
+	(session, W1, b1, W2, b2) = classifier
+	before_relu = np.matmul([test], session.run(W1)) + session.run(b1)
+	return sigmoid(np.matmul(before_relu, session.run(W2)) + session.run(b2))[0][0]
 
-def testData(classifier, test_data):
-	(test_rows, test_cols) = test_data.shape
+def testData(classifier, training_data, test_data):
+	(validation_test_rows, _) = test_data.shape
+	(training_test_rows, _) = training_data.shape 
 
-	is_phishing = test_data[IS_PHISHING_COLUMN_NAME].as_matrix()
-	test_vectors = test_data[FEATURES].as_matrix()
-	urls = test_data[URL_COLUMN_NAME].as_matrix()
+	print "Training tests:"
+	(training_correct_guesses, training_mistake) = testDataInternal(classifier, training_data)
+
+	print "Validation tests:"
+	(validation_correct_guesses, validation_mistake) = testDataInternal(classifier, test_data)
+
+	print "Summary:"
+	print "Validation Tests"
+	print "\tTested #" + str(validation_test_rows) + " URLs"
+	print "\tCorrect guesses: " + str(validation_correct_guesses) + " (" + str((float(validation_correct_guesses) / validation_test_rows) * 100) + "%)"
+	print "\tAggregated mistake from threshold " + str(validation_mistake)
+	print "\tAverage mistake from threshold " + str(float(validation_mistake) / validation_test_rows)
+	print "Training Tests"
+	print "\tTested #" + str(training_test_rows) + " URLs"
+	print "\tCorrect guesses: " + str(training_correct_guesses) + " (" + str((float(training_correct_guesses) / training_test_rows) * 100) + "%)"
+	print "\tAggregated mistake from threshold " + str(training_mistake)
+	print "\tAverage mistake from threshold " + str(float(training_mistake) / training_test_rows)
+
+def testDataInternal(classifier, data):
+	(data_size, _) = data.shape
+
+	is_phishing = data[IS_PHISHING_COLUMN_NAME].as_matrix()
+	test_vectors = data[FEATURES].as_matrix()
+	urls = data[URL_COLUMN_NAME].as_matrix()
+
+	aggregated_threshold_distance_when_wrong = 0
 
 	correct_guesses = 0
-	for i in range(test_rows):
+	for i in range(data_size):
 		prediction = predict(classifier, test_vectors[i]);
 		url = urls[i]
 		is_phishing_url = is_phishing[i]
@@ -167,28 +197,28 @@ def testData(classifier, test_data):
 
 		if is_phishing_url == is_phishing_url_prediction:
 			correct_guesses += 1
+		else:
+			aggregated_threshold_distance_when_wrong += abs(prediction - THRESHOLD)
 
 		print "test url: ", url, "prediction: ", is_phishing_url_prediction, "reality:", is_phishing_url
 
-	print "summary:"
-	print "Tested #" + str(test_rows) + " URLs"
-	print "Correct guesses: " + str(correct_guesses) + " (" + str((float(correct_guesses) / test_rows) * 100) + "%)"
+	return (correct_guesses, aggregated_threshold_distance_when_wrong)
 
 def main():
 	print "Preparing the data..."
 	data = prepareData()
 	print "Preparing the data finished"
 
-	(traning_data, test_data) = splitData(data)
-	print "Training data size:", traning_data.shape
+	(training_data, test_data) = splitData(data)
+	print "Training data size:", training_data.shape
 	print "Test data size:", test_data.shape
 
 	print "Start training"
-	(session, W, b) = generateClassifier(traning_data)
+	(session, W1, b1, W2, b2) = generateNeuralNetworkClassifier(training_data)
 	print "Training ended"
 
 	print "Start testing"
-	testData((session, W, b), test_data)
+	testData((session, W1, b1, W2, b2), training_data, test_data)
 	print "Testing ended"
 
 if __name__ == '__main__':
